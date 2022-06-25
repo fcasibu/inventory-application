91afa09321790340a954be0be74cf50f7ce481c9
nevz <projectnevz@gmail.com>
2022-06-25 22:22:36 +0800

    Add virtual getter for formatted createdAt

--- models/chapter.js
+++ models/chapter.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const ChapterSchema = new mongoose.Schema({
   title: {
@@ -16,11 +17,19 @@ const ChapterSchema = new mongoose.Schema({
   book: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Book'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 });
 
 ChapterSchema.virtual('url').get(function () {
   return `/books/${this.book._id}/chapters/${this._id}`;
+});
+
+ChapterSchema.virtual('createdAt_formatted').get(function () {
+  return DateTime.fromJSDate(this.createdAt).toLocaleString(DateTime.DATE_MED);
 });
 
 module.exports = mongoose.model('Chapter', ChapterSchema);

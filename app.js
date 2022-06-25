2dfe84465156fca3e55620d9bc4a415a0f006cd1
nevz <projectnevz@gmail.com>
2022-06-25 18:23:33 +0800

    Cange extended to true to accept arrays

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/public', express.static(path.join(__dirname, 'public')));
 

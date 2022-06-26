a70ed7c07255948ba73afb105550f12c120aef77
nevz <projectnevz@gmail.com>
2022-06-26 23:54:52 +0800

    Add get/post request for chapter update

--- controllers/chapterController.js
+++ controllers/chapterController.js
@@ -6,10 +6,7 @@ exports.chapter_detail_get = async (req, res, next) => {
   try {
     const chapters = await Chapter.find({ book: req.params.bookId });
     if (!req.query.page) {
-      return res.render('chapter_list', {
-        chapters,
-        bookId: req.params.bookId
-      });
+      return res.redirect(`/books/${req.params.bookId}`);
     }
 
     if (+req.query.page === 0 || Number.isNaN(+req.query.page))
@@ -35,14 +32,14 @@ exports.chapter_detail_get = async (req, res, next) => {
 };
 
 exports.chapter_create_get = (req, res, next) => {
-  res.render('chapter_form');
+  res.render('chapter_form', { title: 'Create Chapter' });
 };
 
 exports.chapter_create_post = [
   body('title', 'A title must have a minimum length of 3')
     .trim()
     .isLength({ min: 3 }),
-  body('text', 'The content must have a minimum length of 10')
+  body('text', 'Content must have a minimum length of 10')
     .trim()
     .isLength({ min: 10 }),
 
@@ -60,11 +57,73 @@ exports.chapter_create_post = [
       });
 
       if (!errors.isEmpty()) {
-        return res.render('chapter_form', { chapter, errors: errors.array() });
+        return res.render('chapter_form', {
+          title: 'Create Chapter',
+          chapter,
+          errors: errors.array()
+        });
       }
 
       await chapter.save();
       res.redirect(`${book.url}/chapters?page=${book.chapter_count + 1}`);
+    } catch (err) {
+      next(err);
+    }
+  }
+];
+
+exports.chapter_update_get = async (req, res, next) => {
+  try {
+    const chapter = await Chapter.findById(req.params.chapterId);
+
+    res.render('chapter_form', {
+      title: 'Update Chapter',
+      chapter,
+      isUpdate: true
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.chapter_update_post = [
+  body('title', 'A title must have a minimum length of 3')
+    .trim()
+    .isLength({ min: 3 }),
+  body('text', 'Content must have a minimum length of 10')
+    .trim()
+    .isLength({ min: 10 }),
+
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      const book = await Book.findById(req.params.bookId);
+      const chapter = new Chapter({
+        title: req.body.title,
+        text: req.body.text,
+        book,
+        _id: req.params.chapterId
+      });
+
+      if (req.body.password !== process.env.PASSWORD) {
+        return res.render('chapter_form', {
+          title: ' Update Chapter',
+          chapter,
+          isUpdate: true,
+          invalidPass: { msg: 'Invalid Password' }
+        });
+      }
+
+      if (!errors.isEmpty()) {
+        return res.render('chapter_form', {
+          title: 'Update Chapter',
+          chapter,
+          errors: errors.array(),
+          isUpdate: true
+        });
+      }
+      await Chapter.findByIdAndUpdate(req.params.chapterId, chapter);
+      res.redirect(`${book.url}`);
     } catch (err) {
       next(err);
     }

cdb000a0ab4621a86b911385041ada840ead84ae
nevz <projectnevz@gmail.com>
2022-06-27 15:53:25 +0800

    Add ThreadComment model

---
+++ models/threadComment.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const ThreadCommentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    minLength: 3,
+    maxLength: 500
+  },
+  thread: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Thread'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('ThreadComment', ThreadCommentSchema);

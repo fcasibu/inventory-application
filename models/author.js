65e7e88477644c0b8dcef7e0ba7f9653d0edfbf6
nevz <projectnevz@gmail.com>
2022-06-27 15:53:48 +0800

    Add virtual for book_list and count

--- models/author.js
+++ models/author.js
@@ -8,13 +8,25 @@ const AuthorSchema = new mongoose.Schema({
     maxLength: [20, 'A name must not exceed 20 characters']
   },
   photoURL: {
-    type: String,
-    default: ''
+    type: String
   }
 });
 
 AuthorSchema.virtual('url').get(function () {
   return `/authors/${this._id}`;
+});
+
+AuthorSchema.virtual('book_list', {
+  ref: 'book',
+  localField: '_id',
+  foreignField: 'author'
+});
+
+AuthorSchema.virtual('book_list_count', {
+  ref: 'book',
+  localField: '_id',
+  foreignField: 'author',
+  count: true
 });
 
 module.exports = mongoose.model('Author', AuthorSchema);

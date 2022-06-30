06a41ca6c15e23bb5143981f77a84f6467be74ce
nevz <projectnevz@gmail.com>
2022-06-30 16:08:29 +0800

    Add middleware for a thread

--- controllers/threadController.js
+++ controllers/threadController.js
@@ -1,6 +1,19 @@
 const { body, validationResult } = require('express-validator');
 const Thread = require('../models/thread');
+const ThreadComment = require('../models/threadComment');
 const Club = require('../models/club');
+
+exports.thread_detail_get = async (req, res, next) => {
+  try {
+    const thread = await Thread.findById(req.params.threadId)
+      .populate('comment_list')
+      .exec();
+
+    res.render('thread_detail', { thread });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.thread_create_get = (req, res, next) => {
   res.render('thread_form', { title: 'Create a Thread' });
@@ -38,6 +51,42 @@ exports.thread_create_post = [
       }
 
       await thread.save();
+      res.redirect(thread.url);
+    } catch (err) {
+      next(err);
+    }
+  }
+];
+
+exports.threadComment_create_post = [
+  body(
+    'text',
+    'A comment must have a min length of 3 characters and a max length of 500 characters'
+  )
+    .trim()
+    .isLength({ min: 3, max: 500 }),
+
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      const thread = await Thread.findById(req.params.threadId)
+        .populate('comment_list')
+        .exec();
+
+      const comment = new ThreadComment({
+        text: req.body.text,
+        thread
+      });
+
+      if (!errors.isEmpty()) {
+        return res.render('thread_detail', {
+          thread,
+          threadComment: comment,
+          errors: errors.array()
+        });
+      }
+
+      await comment.save();
       res.redirect(thread.url);
     } catch (err) {
       next(err);
